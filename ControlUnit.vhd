bd885eeefcf5e0d18ae244c187d5c7af0b0edbf9
lgh <lcliguohao@163.com>
2014-11-27 16:52:47 +0800

    RegDataUnit Done. ControlUnit modified.

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -8,6 +8,7 @@ entity ControlUnit is
     ForceZero  : in  std_logic;
     RegWrite   : out std_logic;
     RegDataSrc : out std_logic_vector(2 downto 0);
+    CmpCode    : out std_logic;
     MemDataSrc : out std_logic;
     MemWrite   : out std_logic;
     MemRead    : out std_logic;
@@ -22,12 +23,13 @@ end ControlUnit;
 
 architecture ControlUnit_Arch of ControlUnit is
 
-  signal output : std_logic_vector(19 downto 0) := X"00000";  -- all signals
+  signal output : std_logic_vector(20 downto 0) := (others => '0');  -- all signals
   
 begin  -- ControlUnit_Arch
 
-  RegWrite <= output(19);
-  RegDataSrc <= output(18 downto 16);
+  RegWrite <= output(20);
+  RegDataSrc <= output(19 downto 17);
+  CmpCode <= output(16);
   MemDataSrc <= output(15);
   MemWrite <= output(14);
   MemRead <= output(13);
@@ -44,69 +46,69 @@ begin  -- ControlUnit_Arch
     else
       case instruc(15 downto 11) is
         when "01001" =>                 -- ADDIU
-          output <= "1000" & "000" & "000001" & "0010000";
+          output <= "10000" & "000" & "000001" & "0010000";
         when "01000" =>                 -- ADDIU3
-          output <= "1000" & "000" & "000001" & "0010001";
+          output <= "10000" & "000" & "000001" & "0010001";
         when "01100" =>
           case instruc(10 downto 8) is
             when "011" =>               -- ADDSP
-              output <= "1000" & "000" & "000001" & "1010011";
+              output <= "10000" & "000" & "000001" & "1010011";
             when "000" =>               -- BTEQZ
-              output <= "0000" & "000" & "101000" & "0000000";
+              output <= "00000" & "000" & "101000" & "0000000";
             when "100" =>               -- MTSP
-              output <= "1101" & "000" & "000000" & "0000011";
+              output <= "11010" & "000" & "000000" & "0000011";
             when others => null;
           end case;
         when "11100" =>
           case instruc(1 downto 0) is
             when "01" =>                -- ADDU
-              output <= "1000" & "000" & "000001" & "0001010";
+              output <= "10000" & "000" & "000001" & "0001010";
             when "11" =>                -- SUBU
-              output <= "1000" & "000" & "000010" & "0001010";
+              output <= "10000" & "000" & "000010" & "0001010";
             when others => null;
           end case;
         when "11101" =>
           case instruc(4 downto 0) is
             when "01100" =>             -- AND
-              output <= "1000" & "000" & "000011" & "0001000";
+              output <= "10000" & "000" & "000011" & "0001000";
             when "01010" =>             -- CMP
-              output <= "1111" & "000" & "000010" & "0001100";
+              output <= "11110" & "000" & "000010" & "0001100";
             when "00000" =>
               case instruc(7 downto 5) is
                 when "000" =>           -- JR
-                  output <= "0000" & "000" & "001000" & "0000000";
+                  output <= "00000" & "000" & "001000" & "0000000";
                 when "010" =>           -- MFPC
-                  output <= "1010" & "000" & "000000" & "0000000";
+                  output <= "10100" & "000" & "000000" & "0000000";
                 when others => null;
               end case;
             when "01101" =>             -- OR
-              output <= "1000" & "000" & "000100" & "0001000";
+              output <= "10000" & "000" & "000100" & "0001000";
             when "01111" =>             -- NOT
-              output <= "1000" & "000" & "000111" & "0101000";
+              output <= "10000" & "000" & "000111" & "0101000";
             when "00100" =>             -- SLLV
-              output <= "1000" & "000" & "000101" & "0100001";
+              output <= "10000" & "000" & "000101" & "0100001";
             when "00111" =>             -- SRAV
-              output <= "1000" & "000" & "000110" & "0100001";
+              output <= "10000" & "000" & "000110" & "0100001";
             when others => null;
           end case;
         when "00010" =>                 -- B
-          output <= "0000" & "000" & "010000" & "0000000";
+          output <= "00000" & "000" & "010000" & "0000000";
         when "00100" =>                 -- BEQZ
-          output <= "0000" & "000" & "011000" & "0000000";
+          output <= "00000" & "000" & "011000" & "0000000";
         when "00101" =>                 -- BNEZ
-          output <= "0000" & "000" & "100000" & "0000000";
+          output <= "00000" & "000" & "100000" & "0000000";
         when "01101" =>                 -- LI
-          output <= "1110" & "000" & "000000" & "0000000";
+          output <= "11100" & "000" & "000000" & "0000000";
         when "10011" =>                 -- LW
-          output <= "1001" & "001" & "000001" & "0010001";
+          output <= "10010" & "001" & "000001" & "0010001";
         when "10010" =>                 -- LW_SP
-          output <= "1001" & "001" & "000001" & "1010000";
+          output <= "10010" & "001" & "000001" & "1010000";
         when "11110" =>
           case instruc(7 downto 0) is
             when X"00" =>               -- MFIH
-              output <= "1011" & "000" & "000000" & "0000000";
+              output <= "10110" & "000" & "000000" & "0000000";
             when X"01" =>               -- MTIH
-              output <= "1100" & "000" & "000000" & "0000101";
+              output <= "11000" & "000" & "000000" & "0000101";
             when others => null;
           end case;
         when "00001" =>                 -- NOP
@@ -114,19 +116,19 @@ begin  -- ControlUnit_Arch
         when "00110" =>
           case instruc(1 downto 0) is
             when "00" =>                -- SLL
-              output <= "1000" & "000" & "000101" & "0110000";
+              output <= "10000" & "000" & "000101" & "0110000";
             when "11" =>                -- SRA
-              output <= "1000" & "000" & "000110" & "0110000";
+              output <= "10000" & "000" & "000110" & "0110000";
             when others => null;
           end case;
         when "11011" =>                 -- SW
-          output <= "0000" & "110" & "000001" & "0010000";
+          output <= "00000" & "110" & "000001" & "0010000";
         when "11010" =>                 -- SW_SP
-          output <= "0000" & "010" & "000001" & "1010000";
+          output <= "00000" & "010" & "000001" & "1010000";
         when "01110" =>                 -- CMPI
-          output <= "1111" & "000" & "000010" & "0010100";
+          output <= "11110" & "000" & "000010" & "0010100";
         when "01010" =>                 -- SLTI
-          output <= "1111" & "000" & "000010" & "0010100";
+          output <= "11111" & "000" & "000010" & "0010100";
         when others => null;
       end case;
     end if;
---
+++ RegDataUnit.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity RegDataUnit is
+  
+  port (
+    RegDataSrc  : in  std_logic_vector(2 downto 0);
+    CmpCode     : in  std_logic;
+    zero        : in  std_logic;
+    neg         : in  std_logic;
+    RegDataCtrl : out std_logic_vector(3 downto 0)
+    );
+
+end RegDataUnit;
+
+architecture RegDataUnit_Arch of RegDataUnit is
+
+begin  -- RegDataUnit_Arch
+
+  RegDataCtrl <= "0111" when RegDataSrc = "111" and CmpCode = '0' and zero = '0' else
+                 "0111" when RegDataSrc = "111" and CmpCode = '1' and neg = '0' else
+                 "1000" when RegDataSrc = "111" and CmpCode = '0' and zero = '1' else
+                 "1000" when RegDataSrc = "111" and CmpCode = '1' and neg = '1' else
+                 "1111";
+
+end RegDataUnit_Arch;
